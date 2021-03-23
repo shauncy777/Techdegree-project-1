8b76ffc8d155d0689dd616ef1787726d243473f4
shauncy777 <75694493+shauncy777@users.noreply.github.com>
2021-03-23 11:42:59 -0700

    Added change to object array
    testing knowledge of Github

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -18,10 +18,10 @@ let quotes = [
    year: "1599 "
   },    
   {  
-    quote: "Cowards die many times before their deaths, The valiant never taste of death but once. ",   
-    source: "William Shakespeare ",
-    citation: "The Tragedy of Julius Caesar ",
-    year: "1599 "
+    quote: "Pain is inevitabe. Suffering is optional.",   
+    source: "Haruki Murakami",
+    citation: "What I Talk About When I Talk About Running",
+    year: "2007"
    }, 
    {  
     quote: "Cowards die many times before their deaths, The valiant never taste of death but once. ",   
