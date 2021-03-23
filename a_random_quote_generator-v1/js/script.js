7eef23cf0ed65587007558108e8be4d286b1c7b2
shauncy777 <75694493+shauncy777@users.noreply.github.com>
2021-03-23 11:18:40 -0700

    Added project files to repo

---
+++ a_random_quote_generator-v1/js/script.js
@@ -0,0 +1,69 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+let quotes = [
+  {  
+   quote: "Cowards die many times before their deaths, The valiant never taste of death but once. ",   
+   source: "William Shakespeare ",
+   citation: "The Tragedy of Julius Caesar ",
+   year: "1599 "
+  },    
+  {  
+    quote: "Cowards die many times before their deaths, The valiant never taste of death but once. ",   
+    source: "William Shakespeare ",
+    citation: "The Tragedy of Julius Caesar ",
+    year: "1599 "
+   }, 
+   {  
+    quote: "Cowards die many times before their deaths, The valiant never taste of death but once. ",   
+    source: "William Shakespeare ",
+    citation: "The Tragedy of Julius Caesar ",
+    year: "1599 "
+   }, 
+   {  
+    quote: "Cowards die many times before their deaths, The valiant never taste of death but once. ",   
+    source: "William Shakespeare ",
+    citation: "The Tragedy of Julius Caesar ",
+    year: "1599 "
+   }, 
+   {  
+    quote: "Cowards die many times before their deaths, The valiant never taste of death but once. ",   
+    source: "William Shakespeare ",
+    citation: "The Tragedy of Julius Caesar ",
+    year: "1599 "
+   }, 
+
+
+
+
+
+];
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
